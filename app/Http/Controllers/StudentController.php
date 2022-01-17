e5015fbb25d5531d0ce63a8903ed48e5cab7e8f9
Nyein Mg <nyeinchanaung.myeik@gmail.com>
2022-01-17 10:10:48 +0630

    Student Controller and routes

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,8 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
-    //
+    public function index(){
+        $students = Student::all();
+        return view('student', ['students' => $students, 'layout' => 'index']);
+    }
+
+    public function create(){
+        $students = Student::all();
+        return view('student', ['students' => $students, 'layout' => 'create']);
+    }
+
+    public function store(){
+        $student = new Student();
+        $student->cne = request()->cne;
+        $student->firstName = request()->firstName;
+        $student->secondName = request()->secondName;
+        $student->age = request()->age;
+        $student->speciality = request()->speciality;
+        $student->save();
+        return redirect('/');
+    }
+
+    public function show($id){
+        $student = Student::find($id);
+        $students = Student::all();
+        return view('student', ['students' => $students, 'student' => $student, 'layout' => 'show']);
+    }
+
+    public function edit($id){
+        $student = Student::find($id);
+        $students = Student::all();
+        return view('student', ['students' => $students, 'student' => $student, 'layout' => 'edit']);
+    }
+
+    public function update($id){
+        $student = Student::find($id);
+        $student->cne = request()->cne;
+        $student->firstName = request()->firstName;
+        $student->secondName = request()->secondName;
+        $student->age = request()->age;
+        $student->speciality = request()->speciality;
+        $student->save();
+        return redirect('/');
+    }
+
+    public function destroy($id){
+        $student = Student::find($id);
+        $student->delete();
+        return redirect('/');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,8 +12,23 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', [StudentController::class, 'index']);
+
+Route::get('/edit/{id}', [StudentController::class, 'edit']);
+
+Route::get('/show/{id}', [StudentController::class, 'show']);
+
+Route::get('/create', [StudentController::class, 'create']);
+
+Route::post('/store', [StudentController::class, 'store']);
+
+Route::post('/update/{id}', [StudentController::class, 'update']);
+
+Route::delete('/delete', [StudentController::class, 'delete']);
